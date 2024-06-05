6524cadc308150be466673ebef73ad71dc7c7e8f
Your Name <you@example.com>
2024-06-05 12:28:34 +0100

    remplir la classe serie

--- src/main/java/com/dao/MovieDao.java
+++ src/main/java/com/dao/MovieDao.java
@@ -1,4 +1,21 @@
 package com.dao;
 
+import com.model.Serie;
+
 public class MovieDao {
+    /**
+     * Hello world!
+     *
+     */
+    public static class app
+    {
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+            Serie produit= new Serie();
+            Serie.
+                      SerieDao produitRepository=new SerieRepository();
+            produitRepository.ajouterSerie(Serie);
+        }
+    }
 }
